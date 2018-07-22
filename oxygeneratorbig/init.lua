bacf974c4e116adefae7b32642fdf686d602c2f9
ManElevation <morenavaj@gmail.com>
2018-07-22 23:17:42 +0200

    Add files via upload

---
+++ oxygeneratorbig/init.lua
@@ -0,0 +1,19 @@
+-- oxygenerator: block that gives breath at times while in radius, mod for minetest
+-- minetest 0.4.17.1
+-- (c) 2018 ManElevation
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+dofile(minetest.get_modpath("oxygeneratorbig").."/big.lua")
+
